de381e622ae245e534702c3b93ed199601c41d4a
Jason Charamis <125705059+JasonCharamis@users.noreply.github.com>
2023-09-01 11:55:46 +0300

    Update gff3lib.py
    Custom library to effectively manipulate gff3 files. 
    Includes functions for sorting gff3 files, extracting chromosome ranges or gene coordinates and converts gff to bed files.

--- gff3lib.py
+++ gff3lib.py
@@ -1,10 +1,8 @@
-import argparse
 from natsort import natsorted
-import re
-import os
+import os, re
+import argparse
 
-
-def check_isfile ( input_file ): ## Function defined outside of class to check if input is a file or a string
+def check_isfile ( input_file ): ## Function to check if input is a file or a string
     if os.path.isfile(input_file):
         glist = []
         
@@ -19,7 +17,7 @@ def check_isfile ( input_file ): ## Function defined outside of class to check i
         return glists
 
 
-class GFF3: ## Define class for GFF3 object
+class GFF3:
     
     __slots__ = [ 'chromosome', 'source', 'type', 'start', 'end', 'score', 'strand', 'phase', 'name' ]
     
@@ -27,37 +25,62 @@ class GFF3: ## Define class for GFF3 object
         self.chromosome = chromosome
         self.source = source
         self.type = type
-        self.start = start
-        self.end = end
+        self.start = int(start)
+        self.end = int(end)
         self.score = score
         self.strand = strand
         self.phase = phase
         self.name = name
 
     def __str__(self):
-        return f"{self.chromosome}\t{self.source} \t {self.type} \t {self.start} \t {self.end} \t {self.score} \t {self.strand} \t {self.phase} \t {self.name}"
+        return f"{self.chromosome}\t{self.source}\t{self.type}\t{self.start}\t{self.end}\t{self.score}\t{self.strand}\t{self.phase}\t{self.name}"
 
-    
-    def parse_gff3 (input_file ): ## opens gff3 file and creates new instance of the gff3 class
+
+    def parse_gff3(input_file):
         gff3_instances = []
         
-        with open ( input_file , 'r' ) as gff3:
+        with open(input_file, 'r') as gff3:
             lines = gff3.readlines()
 
             for line in lines:
                 line = line.strip('\n')
                 comment = re.compile('#')
 
-                if not re.search (comment, line): ## create a new gff3 instance from the files columns and append it into a list
-                    cols = line.split ('\t')
-                    gff3_instance = GFF3( cols[0], cols[1], cols[2], cols[3], cols[4], cols[5], cols[6], cols[7], cols[8] )
+                if not re.search(comment, line):  # Create a new GFF3 instance from the file's columns and append it to a list
+                    cols = line.split('\t')
+                    gff3_instance = GFF3(cols[0], cols[1], cols[2], cols[3], cols[4], cols[5], cols[6], cols[7], cols[8])
                     gff3_instances.append(gff3_instance)
 
-            return gff3_instances  # Return the list of instances
+        return gff3_instances  # Return the list of instances
+
+    
+    def sort_gff3(input_file):
+        gff3_instances = GFF3.parse_gff3(input_file)
+        
+        # Define sorting order for entry types
+        entry_type_order = {
+            "gene": 0,
+            "pseudogene": 1,
+            "mRNA": 2,
+            "transcript": 3,
+            "exon": 4,
+            "CDS": 5,
+            "non_canonical_five_prime_splice_site": 6,
+            "non_canonical_three_prime_splice_site": 7,
+            "stop_codon_read_through": 8
+        }
+        
+        # Define a custom sorting key function
+        def custom_sort_key(element):
+            return (element.chromosome, element.start, entry_type_order.get(element.type, float('inf')))
+        
+        sorted_list = natsorted(gff3_instances, key=custom_sort_key)
+
+        return sorted_list
 
             
     def gff2bed ( input_file, bed_file, only_genes = False ): ## convert gff3 to bed format
-        gff3_instances = GFF3.parse_gff3(input_file)  # Get the list of instances from parse_gff3
+        gff3_instances = GFF3.sort_gff3(input_file)  # Get the list of instances from sort_gff3
 
         for gff3_instance in gff3_instances:
 
@@ -65,13 +88,13 @@ class GFF3: ## Define class for GFF3 object
             gene = re.sub(".*Name=|;.*","", gff3_instance.name)
             gene = re.sub ("-00001","",gene)
                         
-            out = '\t'.join([gff3_instance.chromosome, gff3_instance.start, gff3_instance.end, gene, gff3_instance.score, gff3_instance.strand] )
+            out = ''.join([gff3_instance.chromosome, gff3_instance.start, gff3_instance.end, gene, gff3_instance.score, gff3_instance.strand] )
                 
-            write_file ( out, args.bed )
+        return out
 
             
     def extract_range (input_file, chromosome, start, end ): ## extract range from gff3 file
-        gff3_instances = GFF3.parse_gff3(input_file)  # Get the list of instances from parse_gff3
+        gff3_instances = GFF3.sort_gff3(input_file)  # Get the list of instances from sort_gff3
         
         subset = []
         out = str ()
@@ -85,19 +108,20 @@ class GFF3: ## Define class for GFF3 object
                 if gff3_instance.start >= start and gff3_instance.end <= end:
                     subset.append ( gff3_instance )
 
-        inp = re.sub(".gff3","",input_file)
+        inp = re.sub(".gff$|.gff3$","",input_file)
 
         with open(f"{inp}_extracted_{start}_{end}.gff3", "w") as f:
             for out in subset:
                 print ( out, file = f)
 
-     
+        return
+
+    
     def extract_genes ( input_file, gene_list, coords = True ): ## extract gene coordinates from gff3 file        
         subset = []
         found = False
 
-        gff3_instances = GFF3.parse_gff3(input_file)  # Get the list of instances from parse_gff3
-
+        gff3_instances = GFF3.sort_gff3(input_file)  # Get the list of instances from sort_gff3
 
         for gff3_instance in gff3_instances:            
             if re.search("gene",gff3_instance.type):
@@ -119,70 +143,86 @@ class GFF3: ## Define class for GFF3 object
                 subset.append ( gff3_instance )
 
                 
-        if len(subset) > 0:
-            
-            # Define sorting order for entry types
-            entry_type_order = {"gene":0,"pseudogene":1,"mRNA":2,"transcript":3,"exon": 4, "CDS": 5,"stop_codon_read_through":6}
-        
-            # Define a custom sorting key function
-            def custom_sort_key(element):
-                return (element.chromosome,element.start, entry_type_order[element.type])
-
-            sorted_list = natsorted(subset, key=custom_sort_key)  # Use the custom sorting function
+        if len(subset) > 0:            
 
             inp = re.sub ( ".gff3", "", input_file )
+            beds = []
 
             if coords == True:
-                with open(f"{inp}_extracted_{gene_list}.gff3", "w") as f:
-                    for out in sorted_list:
-                        print ( out, file = f )
+                return sorted_list
+
             else:
                 with open(f"{inp}_extracted_{gene_list}.bed", "w") as f:
                     for out in sorted_list:
                         if re.search("gene", out.type ):
                             out.name = re.sub (".*Name=|\.t\d|;.*","",out.name)
                             bed = '\t'.join([out.name, out.chromosome, out.start, out.end, out.strand])
-                            print ( bed, file = f )
+                            beds.append(bed)
+
+                        return beds
 
         else:
             print ( "Gene list is empty or not provided." )
 
+
+
            
 ## Implementation ##
 
-def parse_args(): # Parse command-line arguments
+def kargs():
+    # Parse command-line arguments
     parser = argparse.ArgumentParser(description='Library for efficiently manipulating gff3 files.')
     parser.add_argument('--gff3', type=str, help='GFF3 file that you would like to sort.')
-    parser.add_argument('--bed', type=str, help='BED output file.')
+    parser.add_argument('--bed', action="store_true",help='BED output file.')
+    parser.add_argument('-e','--extract', action="store_true", help='Option to extract range from GFF3 file.')
+    parser.add_argument('-s','--sort', action="store_true", help='Option to sort the GFF3 file.')
     parser.add_argument('-l', '--gene_list', type=str, help='List with gene IDs that you would like to extract.')
-    parser.add_argument('--extract', type=str, help='Option to extract range from GFF3 file.')
-    parser.add_argument('--coords', type=str, help='Option to extract bed-like features for selected genes.')
-    parser.add_argument('--chromosome', type=str, help='Chromosome whose range you would like to extract.')
-    parser.add_argument('--start', type=str, help='Start position of the range you would like to extract.')
-    parser.add_argument('--end', type=str, help='End position of the range you would like to extract.')
-    return parser.parse_args()
-
-def main():
-    args = parse_args()
+    parser.add_argument('-c','--coords', type=str, help='Option to extract bed-like features for selected genes.')
+    parser.add_argument('-r','--range', action="store_true", help='Option to extract range from GFF3 file.')
+    parser.add_argument('-chr','--chromosome', type=str, help='Chromosome whose range you would like to extract.')
+    parser.add_argument('-st','--start', type=str, help='Start position of the range you would like to extract.')
+    parser.add_argument('-end','--end', type=str, help='End position of the range you would like to extract.')
+    args = parser.parse_args()
 
     if not any(vars(args).values()):
         parser.print_help()
         print("Error: No arguments provided.")
-        return
 
+    return parser.parse_args()
+
+
+def main():
+    
+    args = kargs()
+    
     if args.gff3:
-        if args.extract:
-            GFF3.extract_range(args.gff3, args.chromosome, args.start, args.end)
-        elif args.gene_list:
-            GFF3.extract_genes(args.gff3, args.gene_list)
-        elif args.gene_list and args.coords:
-            GFF3.extract_genes(args.gff3, args.gene_list, args.coords)
-        elif args.gff2bed:
-            GFF3.gff2bed(args.gff3, args.bed)
+
+        inp = re.sub (".gff$|.gff3$","",args.gff3)
+        
+        if args.sort:
+            with open(f"{inp}.sorted.gff3", "w") as f:
+                for out in GFF3.sort_gff3(args.gff3):
+                    print ( out, file = f )
+                    
+        elif args.extract:
+            with open(f"{inp}_extracted_{gene_list}.gff3", "w") as f:
+                for out in GFF3.extract_genes(args.gff3, args.gene_list):
+                    print ( out, file = f )
+
+        elif args.range:
+            with open(f"{inp}_extracted_{args.start}_{args.end}.gff3", "w") as f:
+                for out in GFF3.extract_range(args.gff3, args.chromosome, args.start, args.end):
+                    print ( out, file = f )
+                    
+        elif args.bed:
+            with open(f"{inp}.bed", "w") as f:
+                for out in GFF3.gff2bed(args.gff3):
+                    print ( out, file = f )                  
         else:
-            print("Please provide either --extract, --bed or --gene_list option.")
+            print("Please provide either --sort, --extract, --range, --bed or --gene_list option.")
+            
     else:
-        print("Please provide a gff3 file as input.")
+        print ("Please provide a gff3 file as input.")
             
 
 if __name__ == "__main__":
